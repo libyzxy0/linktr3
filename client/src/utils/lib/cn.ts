22cbf3d35a3b0721e2a9f55e49b15aa388913691
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2024-06-02 18:11:50 +0800

    fix: ts

--- client/src/utils/lib/cn.ts
+++ client/src/utils/lib/cn.ts
@@ -1,6 +1,6 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export const cn = (...inputs) => {
+export const cn = (...inputs: (string | undefined)[]) => {
   return twMerge(clsx(inputs));
 };
