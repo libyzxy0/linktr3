42c19c17d9da9dd302af47d4d2d7b325f4e605c6
libyzxy0 <janlibydelacosta@gmail.com>
2024-06-06 18:40:30 +0800

    fix: cloudinary type

--- client/src/utils/lib/cld.ts
+++ client/src/utils/lib/cld.ts
@@ -1,31 +1,34 @@
-import { v2 as cloudinary } from "cloudinary";
+import { v2 as cloudinary, UploadApiResponse, UploadApiErrorResponse } from "cloudinary";
 import type { CloudinaryResponseType } from '@/types';
 
+// Configure Cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const upload = async (buffer: any): Promise<CloudinaryResponseType> => {
-  const data = await new Promise((resolve, reject) => {
-    cloudinary.uploader
-      .upload_stream(
-        {
-          tags: ["ml_default"],
-          upload_preset: "ml_default",
-        },
-        function (error, result) {
-          if (error) {
-            reject(error);
-            return;
-          }
-          resolve(result);
-        },
-      )
-      .end(buffer);
+
+const upload = async (buffer: Buffer): Promise<CloudinaryResponseType> => {
+  const data = await new Promise<UploadApiResponse>((resolve, reject) => {
+    const uploadStream = cloudinary.uploader.upload_stream(
+      {
+        tags: ["ml_default"],
+        upload_preset: "ml_default",
+      },
+      (error: UploadApiErrorResponse | undefined, result: UploadApiResponse | undefined) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        resolve(result);
+      }
+    );
+
+    uploadStream.end(buffer);
   });
-  return data;
+
+  return data as CloudinaryResponseType;
 };
 
 export { cloudinary, upload };
